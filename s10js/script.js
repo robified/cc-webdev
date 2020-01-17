e5c9a3aba334caa996c0a450c0e42d59fe6d5047
Robin <daorob@gmail.com>
2020-01-17 06:29:37 -0600

    Complete and add section 10 notes.

---
+++ s10js/script.js
@@ -0,0 +1,16 @@
+// useful built-in methods
+// var userName = prompt('What is your name?');
+// alert('Nice to meet you, ' + userName + '!');
+// console.log('Also great to meet you, ' + userName + '!');
+
+// js stalker exercise
+// var firstName = prompt('What is your first name?');
+// var lastName = prompt('What is your last name?');
+// var age = prompt('How old are you?');
+// console.log('Your full name is ' + firstName + ' ' + lastName);
+// console.log('You are ' + age + ' years old');
+
+// age calculator
+var age = prompt('How old are you?');
+var days = age * 365.25;
+console.log(age + ' years is roughly ' + days + ' days');
